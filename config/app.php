c96ca9f36c92d27e0f081db17cb6495851bbfcb9
AQUACY <godwinboadi@gmail.com>
2025-02-01 15:42:28 +0000

    another change

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://greenpathcasemanager.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -163,5 +163,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
 
     ],
+
+    'asset_url' => env('ASSET_URL', null),
 
 ];
